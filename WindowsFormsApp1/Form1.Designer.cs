826c08fd799511bff7edc070594bd1a63b54bda8
mauriani <maurianim@gmail.com>
2022-05-27 12:01:36 -0300

    Adicionando buttons  - Arquivos

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,15 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblHelloWorld = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.novoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salvarTextoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copiarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refazerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.desfazerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSalvar = new System.Windows.Forms.ToolStripButton();
+            this.buttonAbrirArquivo = new System.Windows.Forms.Button();
+            this.lblHelloWorld = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(788, 269);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.novoToolStripMenuItem,
+            this.abrirTextoToolStripMenuItem,
+            this.salvarTextoToolStripMenuItem,
+            this.copiarToolStripMenuItem,
+            this.colarToolStripMenuItem,
+            this.refazerToolStripMenuItem,
+            this.desfazerToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.arquivoToolStripMenuItem.Text = "Arquivo";
+            // 
+            // novoToolStripMenuItem
+            // 
+            this.novoToolStripMenuItem.Name = "novoToolStripMenuItem";
+            this.novoToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.novoToolStripMenuItem.Text = "Novo";
+            // 
+            // abrirTextoToolStripMenuItem
+            // 
+            this.abrirTextoToolStripMenuItem.Name = "abrirTextoToolStripMenuItem";
+            this.abrirTextoToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.abrirTextoToolStripMenuItem.Text = "Abrir Texto";
+            // 
+            // salvarTextoToolStripMenuItem
+            // 
+            this.salvarTextoToolStripMenuItem.Name = "salvarTextoToolStripMenuItem";
+            this.salvarTextoToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.salvarTextoToolStripMenuItem.Text = "Salvar Texto";
+            // 
+            // copiarToolStripMenuItem
+            // 
+            this.copiarToolStripMenuItem.Name = "copiarToolStripMenuItem";
+            this.copiarToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.copiarToolStripMenuItem.Text = "Copiar";
+            // 
+            // colarToolStripMenuItem
+            // 
+            this.colarToolStripMenuItem.Name = "colarToolStripMenuItem";
+            this.colarToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.colarToolStripMenuItem.Text = "Colar";
+            // 
+            // refazerToolStripMenuItem
+            // 
+            this.refazerToolStripMenuItem.Name = "refazerToolStripMenuItem";
+            this.refazerToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.refazerToolStripMenuItem.Text = "Refazer";
+            // 
+            // desfazerToolStripMenuItem
+            // 
+            this.desfazerToolStripMenuItem.Name = "desfazerToolStripMenuItem";
+            this.desfazerToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.desfazerToolStripMenuItem.Text = "Desfazer";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButtonSalvar});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Novo arquivo";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "Abrir arquivo";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButtonSalvar
+            // 
+            this.toolStripButtonSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonSalvar.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSalvar.Image")));
+            this.toolStripButtonSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSalvar.Name = "toolStripButtonSalvar";
+            this.toolStripButtonSalvar.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonSalvar.Text = "Salvar arquivo";
+            this.toolStripButtonSalvar.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // buttonAbrirArquivo
+            // 
+            this.buttonAbrirArquivo.Location = new System.Drawing.Point(12, 402);
+            this.buttonAbrirArquivo.Name = "buttonAbrirArquivo";
+            this.buttonAbrirArquivo.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbrirArquivo.TabIndex = 2;
+            this.buttonAbrirArquivo.Text = "Abrir arquivo";
+            this.buttonAbrirArquivo.UseVisualStyleBackColor = true;
+            this.buttonAbrirArquivo.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // lblHelloWorld
             // 
-            this.lblHelloWorld.Location = new System.Drawing.Point(634, 303);
+            this.lblHelloWorld.Location = new System.Drawing.Point(634, 402);
             this.lblHelloWorld.Name = "lblHelloWorld";
             this.lblHelloWorld.Size = new System.Drawing.Size(75, 23);
             this.lblHelloWorld.TabIndex = 0;
@@ -44,51 +193,51 @@
             this.lblHelloWorld.UseVisualStyleBackColor = true;
             this.lblHelloWorld.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // saveFileDialog1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(697, 261);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 303);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Adicionar arquivo";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
-            // 
-            // openFileDialog
-            // 
-            this.openFileDialog.FileName = "openFileDialog1";
-            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonAbrirArquivo);
             this.Controls.Add(this.lblHelloWorld);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button lblHelloWorld;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem novoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salvarTextoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copiarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refazerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem desfazerToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSalvar;
+        private System.Windows.Forms.Button buttonAbrirArquivo;
+        private System.Windows.Forms.Button lblHelloWorld;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Collections.Generic;
+using System.IO;
 
 namespace WindowsFormsApp1
 {
@@ -18,12 +18,75 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
+        // FUNÇÕES
+
+        private void salva_arquivo_lista()
+        {
+            string text = richTextBox1.Text;
+            char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
+            string[] words = text.Split(delimiterChars);
+            List<string> dicionario = new List<string>();
+
+            foreach (var word in words)
+            {
+                System.Console.WriteLine(word);
+
+                if (word.Length > 0)
+                {
+                    dicionario.Add(word);
+                }
+            }
+
+            Console.WriteLine(dicionario);
+        }
+
+        private void alert_Salvar()
+        {
+            // BUTTON NOVO ARQUIVO
+            // PRIMEIRO VAMOS FAZER UM ALERT
+            if (!string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                if ((MessageBox.Show("Deseja Salvar o arquivo ?", "Salvar ", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes))
+                {
+                    salvar_Arquivo();
+                }
+            }
+        }
+
+        private void salvar_Arquivo()
+        {
+            try
+            {
+                // ABRE A JANELA DO COMPUTADOR PARA ESCOLHER ONDE SALVAR O ARQUIVO
+                if(this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // abre um stream para escrita e cria um StreamWriter para implementar o stream
+                    FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter m_streamWriter = new StreamWriter(fs);
+                    m_streamWriter.Flush();
+                    // Escreve para o arquivo usando a classe StreamWriter
+                    m_streamWriter.BaseStream.Seek(0, SeekOrigin.Begin);
+                    // escreve no controle richtextbox
+                    m_streamWriter.Write(this.richTextBox1.Text);
+                    // fecha o arquivo
+                    m_streamWriter.Flush();
+                    m_streamWriter.Close();
+                }
+
+            }
+            catch(Exception error)
+            {
+                MessageBox.Show("Atenção" + error.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // QUANDO O USUÁRIO INSERE O TEXTO
         private void button1_Click(object sender, EventArgs e)
         {
             // CORRIGIR A LISTA, VINDO COM ESPAÇO
 
-            // QUANDO O USUÁRIO INSERE O TEXTO
-            string text = textBox1.Text;
+            
+            string text = richTextBox1.Text;
             char[] delimiterChars = { ' ', ',', '.', ':', '\t' };
             string[] words = text.Split(delimiterChars);
             List<string> dicionario = new List<string>();
@@ -43,11 +106,6 @@ namespace WindowsFormsApp1
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void button1_Click_1(object sender, EventArgs e)
         {
             
@@ -55,10 +113,88 @@ namespace WindowsFormsApp1
 
         private void openFileDialog_FileOk(object sender, CancelEventArgs e)
         {
-            var abrirArquivo = new OpenFileDialog();
-            abrirArquivo.Title = "Selecione um arquivo";
-            abrirArquivo.Filter = "txt file|*txt";
-            abrirArquivo.RestoreDirectory = true;
+          
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // NOVO ARQUIVO
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            alert_Salvar();
+            richTextBox1.Focus();
+            richTextBox1.Clear();
+        }
+
+        // ABRIR ARQUIVO EXISTENTE
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            // isso afirmar que o arquivo pode ser multilinhas
+            this.openFileDialog.Multiselect = true;
+
+            // selecionar mais de um arquivo e FileNames
+            this.openFileDialog.Title = "Selecione o arquivo";
+
+            // Diretório a ser exibido quando a janela de diálogo aparecer pela primeira vez
+            openFileDialog.InitialDirectory = @"C:\";
+
+            // FILTRA APENAS ARQUIVOS DE TEXTO
+            openFileDialog.Filter = "Images (*.TXT)|*.TXT|" + "All files (*.*)|*.*";
+
+            // Representa o indíce do filtro atualmetne selecionado na janela de diálogo
+            openFileDialog.FilterIndex = 1;
+
+            // Indica se a caixa de verificação estará selecionada
+            this.openFileDialog.ReadOnlyChecked = true;
+
+
+            openFileDialog.CheckFileExists = true;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.ShowReadOnly = true;
+
+            DialogResult dr = this.openFileDialog.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    FileStream fs = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read);
+                    StreamReader m_streamReader = new StreamReader(fs);
+                    // Lê o arquivo usando a classe StreamReader
+                    m_streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    // Lê cada linha do stream e faz o parse até a última linha
+                    this.richTextBox1.Text = "";
+                    string strLine = m_streamReader.ReadLine();
+                    while (strLine != null)
+                    {
+                        this.richTextBox1.Text += strLine + "\n";
+                        strLine = m_streamReader.ReadLine();
+                    }
+                    // Fecha o stream
+                    m_streamReader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+        
+        // BUTTON MENU SALVAR ARQUIVO
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
         }
     }
 }
